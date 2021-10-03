c0cd8198a28af0b512d702f3d0d6ed8292a74119
Yalegama <nadunyalegama@gmai.com>
2021-10-03 23:33:39 +0530

    fourth commit

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 <a href="https://jsonplaceholder.typicode.com/"> https://jsonplaceholder.typicode.com/ </a>
 </li>
 </ul>
+<hr>
+<img src="./images/screen1.png">
+<img src="./images/screen2.png">
+<img src="./images/screen3.png">
+<img src="./images/screen4.png">
--- script.js
+++ script.js
@@ -25,9 +25,9 @@ function getUsers(){
         let output=`<h1>Users Details</h1>`
 
         users.forEach((value,index,array)=>{
-            output+=`<ul>
-                        <li>Name is: ${value.name}</li>
-                        <li>Email is: ${value.email}</li>
+            output+=`<ul class="list-group mb-3">
+                        <li class="list-group-item">Name is: ${value.name}</li>
+                        <li class="list-group-item">Email is: ${value.email}</li>
                     </ul>`
         })
 
@@ -47,11 +47,11 @@ function getPosts(){
         fetch('https://jsonplaceholder.typicode.com/posts')
         .then((res)=>res.json())
         .then((users)=>{
-            let output=`<h1>Users</h1>`
+            let output=`<h1 class="mb-4">Users</h1>`
             users.forEach((value,index,array)=>{
                 output+=`
                 
-                <div>
+                <div class="card card-body mb-3">
                 
                 <h2>User Id: ${value.userId}</h2>
                 <h2>Id: ${value.id}</h2>
@@ -65,4 +65,30 @@ function getPosts(){
             document.getElementById('output').innerHTML=output;
         })
         .catch((err)=>console.log("Error"))
+}
+
+
+/***Form  */
+
+document.getElementById('addPost').addEventListener('submit',addThisPost);
+
+function addThisPost(e){
+    e.preventDefault();
+    let title=document.getElementById('title').value;
+    let body=document.getElementById('body').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts'),{
+        method:'POST',
+        headers:{
+            'Accept':'application/json, text/plain, */*',
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify({
+            title:title,
+            body:body
+        })
+        .then((res)=>res.json())
+        .then((data)=>console.log(data))
+    }
+
 }
